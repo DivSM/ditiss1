441f13f3731f2889efbf609076370f5c050a0366
DivSM <divya9light@gmail.com>
2022-09-07 19:43:17 +0530

    userinfo with shell info

--- uname.sh
+++ uname.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 _user="$(id -u -n)"
 _uid="$(id -u)"
+_sinfo="ps -p $$"
+_shinfo="$SHELL"
 echo "User name : $_user"
 echo "User name ID (UID) : $_uid"
+echo "Shell info : $_sinfo"
+echo "Shell info : $_shinfo"
